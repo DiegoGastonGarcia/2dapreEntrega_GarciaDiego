cbbf24ebb6dcb2aec0b5c670372162df280c416e
Diego Garcia <d.g.garcia@gmail.com>
2023-11-04 05:21:31 -0300

    funcion agregar completa

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
-import juegos from "./juegos.json" assert { type: "json" };
+let gameDiv = document.getElementById("containerJuegos");
+let agregarJuegoBtn = document.getElementById("agregarBtn");
+let eliminarJuegoBtn = document.getElementById("deleteBtn");
 
-function mostrarJuego(containerGames) {
-  for (let juego of juegos) {
+function mostrarJuego(array) {
+  gameDiv.innerHTML = "";
+  for (let juego of biblioteca) {
     let cardJuego = document.createElement("div");
     cardJuego.className = "col-4";
     cardJuego.innerHTML = `
     <div class="card" style="width: 18rem;">
-                    <img src="./assets/images/${juego.imagen}" class="card-img-top" alt="No cargo la imagen papi">
+                    <img src="./assets/images/${juego.imagen}" class="card-img-top" alt="No cargo la imagen pa">
                     <div class="card-body">
                       <h5 class="card-title">${juego.nombre}</h5>
                       <p class="card-text">Pertenece al genero de ${juego.genero} y la duracion promedio de una partida es de ${juego.duracion} minutos.</p>
-                      <button class="btn btn-primary">Eliminar Juego</button>
+                      <button class="btn btn-primary" id="deleteBtn" >Eliminar Juego</button>
                     </div>
                   </div>`;
-    containerGames.append(cardJuego);
+    gameDiv.append(cardJuego);
   }
 }
 
@@ -21,7 +24,7 @@ function juegoNuevo(array) {
   let nombre = document.getElementById("nombreJuego");
   let genero = document.getElementById("generoJuego");
   let duracion = document.getElementById("duracionJuego");
-  const nuevoJuego = new juego(
+  const nuevoJuego = new Juego(
     array.length + 1,
     nombre.value,
     genero.value,
@@ -32,10 +35,15 @@ function juegoNuevo(array) {
   nombre.value = "";
   genero.value = "";
   duracion.value = "";
+  localStorage.setItem("bliblioteca", JSON.stringify(biblioteca));
 }
 
+agregarJuegoBtn.addEventListener("click", () => {
+  console.log("escucho el boton");
+  juegoNuevo(biblioteca);
+  mostrarJuego(biblioteca);
+});
+
 document.addEventListener("DOMContentLoaded", (event) => {
-  console.log("me lo escucho");
-  let containerGames = document.getElementById("games");
-  mostrarJuego(containerGames);
+  mostrarJuego(biblioteca);
 });
---
+++ modulo.js
@@ -0,0 +1,110 @@
+// este simulador va a estar diseñado para recomendar un juego cuando disponemos de un tiempo especifico para jugar, debido a que una de las problematicas actuales es tener muchos juegos en la biblioteca pero poco tiempo para jugarlos
+
+class Juego {
+  constructor(id, nombre, genero, duracion, imagen) {
+    (this.id = id),
+      (this.nombre = nombre),
+      (this.duracion = genero),
+      (this.genero = duracion);
+    this.imagen = imagen;
+  }
+}
+
+let cargarBiblioteca = async () => {
+  const resp = await fetch("games.json");
+  const dataGame = await resp.json();
+  for (let game of dataGame) {
+    let newgame = new Juego(
+      game.id,
+      game.nombre,
+      game.genero,
+      game.duracion,
+      game.imagen
+    );
+    biblioteca.push(newgame);
+  }
+  localStorage.setItem("biblioteca", JSON.stringify(biblioteca));
+  console.log(cargarBiblioteca);
+};
+
+// juegos precargados para la simulacion (algunos son de mi gusto personal, otros de consumo masivo)
+let biblioteca = [];
+if (localStorage.getItem("biblioteca")) {
+  for (let game of JSON.parse(localStorage.getItem("biblioteca"))) {
+    let gameLocal = new Juego(
+      game.id,
+      game.nombre,
+      game.genero,
+      game.duracion,
+      game.imagen
+    );
+    biblioteca.push(gameLocal);
+  }
+} else {
+  cargarBiblioteca();
+  console.log(biblioteca);
+}
+
+// consultar que jugar hoy con mi tiempo disponible
+// function consultarJuego(array) {
+//   let tiempoDisponible = prompt(
+//     "Ingrese el tiempo con el que dispone expresado en minutos (solo valores númericos)"
+//   );
+//   let busqueda = array.filter((elem) => elem.tiempoP <= tiempoDisponible);
+//   // const recomendacion =
+//   if (busqueda.length == 0) {
+//     console.log(`No hay coincidencias con ${tiempoDisponible}`);
+//   } else {
+//     mostrarJuegos(busqueda);
+//   }
+// }
+
+// Consultar un juego por el genero al que pertenece
+// function buscarCategoria(array) {
+//   let genero = prompt("Ingrese a que genero de juegos quisiera jugar");
+//   let generoBuscado = array.filter((juegos) =>
+//     juegos.categoria.includes(genero.toLowerCase())
+//   );
+//   if (generoBuscado.length == 0) {
+//     console.log(
+//       `No encontramos coincidencias con el genero deseado: ${genero}`
+//     );
+//   } else {
+//     mostrarJuegos(generoBuscado);
+//   }
+// }
+
+// function mostrarId(array) {
+//   console.log(array);
+//   console.log("Cual es el ID?");
+//   for (let juego of array) juego.idJuego();
+// }
+
+// function eliminarJuego(array) {
+//   mostrarId(array);
+//   let eliminarId = parseInt(
+//     prompt(
+//       "Ingrese el ID del juego indicado en la consola (solo valores númericos)"
+//     )
+//   );
+//   const coincidencia = array.find((elem) => elem.id === eliminarId);
+//   if (!coincidencia) {
+//     alert(`El id ingresado no existe`);
+//     return;
+//   }
+//   let preguntaID = parseInt(
+//     prompt(`Usted desea eliminar el juego ${coincidencia.nombre} 1: Si 2: No`)
+//   );
+//   switch (preguntaID) {
+//     case 1:
+//       let idBuscada = array.filter((elem) => elem.id !== eliminarId);
+//       biblioteca = idBuscada;
+//       alert("Usted eliminó el juego ingresado");
+//       console.log(biblioteca);
+//       break;
+//     case 2:
+//       break;
+//     default:
+//       alert("Ingrese una de las opciones detalladas (solo valores numéricos)");
+//   }
+// }
